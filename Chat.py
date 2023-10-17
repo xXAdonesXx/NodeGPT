d26bdb383edcd1cf10662c5f7eb7401b6fb42538
Adones <66518617+xXAdonesXx@users.noreply.github.com>
2023-10-18 00:42:56 +0200

    Add files via upload

--- Chat.py
+++ Chat.py
@@ -4,6 +4,7 @@ base_dir = os.path.dirname(os.path.abspath(__file__))
 venv_site_packages = os.path.join(base_dir, 'venv', 'Lib', 'site-packages')
 sys.path.append(venv_site_packages)
 import autogen
+import json
 
 
 class Chat:
