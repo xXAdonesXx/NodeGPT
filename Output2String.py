566638e08208453a3a5261f64f318b24ddd8f61c
Adones <66518617+xXAdonesXx@users.noreply.github.com>
2023-10-19 22:59:10 +0200

    Add files via upload

---
+++ Output2String.py
@@ -0,0 +1,34 @@
+import json
+class Output2String:
+    def __init__(self):
+        self.type = "output"
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "text": ("TEXT",),
+            },
+            "hidden": {},
+        }
+
+    RETURN_TYPES = ("STRING",)
+    FUNCTION = "execute"
+    CATEGORY = "AutoGen"
+
+    def execute(self, text):
+
+        text = text['TEXT']
+        print("------------------------------------------------")
+        print("Last Response:")
+        print("------------------------------------------------")
+        print(text)
+
+        return text
+
+NODE_CLASS_MAPPINGS = {
+    "Output2String": Output2String,
+}
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "Output2String": "Output2String"
+}
