93a2ca825d58f8da6d43a07c5732ef40483aa73a
flossverse <github@xrsystems.uk>
2023-10-20 14:29:59 +0100

    Multi OS compatitbiliy

--- Agents/Scientist.py
+++ Agents/Scientist.py
@@ -4,7 +4,21 @@ base_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(base_dir)
 venv_site_packages = os.path.join(parent_dir, 'venv', 'Lib', 'site-packages')
 sys.path.append(venv_site_packages)
-import autogen
+
+try:
+    import autogen
+except ImportError:
+    import sys
+    import os
+
+    # Determine the correct path based on the operating system
+    if os.name == 'posix':
+        site_packages = os.path.join(sys.prefix, 'lib', 'python{}.{}/site-packages'.format(sys.version_info.major, sys.version_info.minor))
+    else:  # For Windows
+        site_packages = os.path.join(sys.prefix, 'Lib', 'site-packages')
+
+    sys.path.append(site_packages)
+    import autogen
 
 class Scientist:
     @classmethod
