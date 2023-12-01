0ef2c695df244072cb23addba93ff39d368968a3
xXAdonesXx <adones.van.eckengoed@gmail.com>
2023-12-01 14:28:33 +0100

    installation from the start

--- __init__.py
+++ __init__.py
@@ -31,6 +31,18 @@ def install_package():
 
     subprocess.run(f"{venv_activate} && {requirements_install}", shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
 
+def install_package2():
+    # Navigate to the NodeGPT directory
+    os.chdir(base_dir)
+
+    # Run the install.bat file
+    subprocess.run('install.bat', shell=True)
+
+    venv_activate = ".\\venv\\Scripts\\activate"
+    requirements_install = "pip install -r requirements.txt"
+
+    subprocess.run(f"{venv_activate} && {requirements_install}", shell=True)
+
 def read_config(file_path):
     with open(file_path, 'r') as file:
         return json.load(file)
@@ -47,12 +59,37 @@ def update_repository(repo_path):
     except Exception as e:
         print(f"An error occurred while updating the repository: {e}")
 
+
+def update_config(file_path, auto_update_value):
+    try:
+        # Read the existing JSON file
+        with open(file_path, 'r') as file:
+            config = json.load(file)
+
+        # Update the AutoUpdate value
+        config['first_installation'] = auto_update_value
+
+        # Write the updated config back to the file
+        with open(file_path, 'w') as file:
+            json.dump(config, file, indent=4)  # Indent for pretty printing
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
 auto_update_path = os.path.join(base_dir, 'AutoUpdate.json')
 print(auto_update_path)
 config = read_config(auto_update_path)
 auto_update = config.get('AutoUpdate', False)
+first_install = config.get('first_installation', False)
 
-
+if first_install:
+    try:
+        install_package2()
+        print("NodeGPT installed")
+    except Exception as e:
+        print(f"An error occurred during installation: {e}")
+        print("Something went wrong. Please try running the Update.bat inside ComfyUI/custom_nodes/NodeGPT. If the error still remains, please open an issue on https://github.com/xXAdonesXx/NodeGPT")
+    update_config(auto_update_path, False)
 
 if auto_update:
     try:
